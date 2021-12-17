f077683846254d34f0d2ea6c70fc1ff97b34ec26
Com-Sun <gnlslfflffl@gmail.com>
2021-12-17 11:06:21 +0900

    fix: login css

--- frontMiniProject/js/login.js
+++ frontMiniProject/js/login.js
@@ -1,3 +1,5 @@
+$(document).ready(function() { alert('jQuery 사용 알림'); });
+
 document.getElementById("submit").onclick = function () {
     let id = document.getElementById("username").value;
     let pw = document.getElementById("password").value;
